01b8acbc41ed3de772abcf366618b8ae603ce661
bmalbright <brianalbright@utexas.edu>
2022-01-26 14:47:27 -0600

    updated the connection.js file.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/jester', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://mongodb+srv://user-1:jester#1@cluster0.bg2a7.mongodb.net/jester?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
