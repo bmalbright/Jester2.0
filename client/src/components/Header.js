17b20c023e2b5afad8b37957b9e02e7c5f8dfdc1
bmalbright <brianalbright@utexas.edu>
2022-01-25 23:59:37 -0600

    small edits. Trying to debug login/signup function.

---
+++ client/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+// import Icon from "../images/JesterLogo2.g";
+
+
+function appHeader() {
+  return (
+    <div>
+      <div className="header">
+        <Card  className="headercard">
+          {/* <img  className="fluid" src={Icon} alt="jester icon" ></img> */}
+        </Card>
+      </div>
+    </div>
+  );
+}
+
+export default appHeader;
