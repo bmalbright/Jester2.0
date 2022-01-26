87c90b2939391a02762da317bf30ff4659cb3f7e
bmalbright <brianalbright@utexas.edu>
2022-01-26 15:26:04 -0600

    editing the connection.js file. Deployment to heroku keeps failing.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://user-1:#1@cluster0.bg2a7.mongodb.net/jester?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://user-1:#1@cluster0.bg2a7.mongodb.net/jester?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   // useCreateIndex: true,
